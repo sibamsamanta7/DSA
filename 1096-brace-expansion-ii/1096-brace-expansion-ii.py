dbb40e33d559fff8036cc3f96b52f5513cb5c7ce
sibamsamanta7 <sibamsamanta7@gmail.com>
2026-09-25 11:46:42 +0530

    Time: 22 ms (5.46%), Space: 19.9 MB (10.38%) - LeetHub

--- 1096-brace-expansion-ii/1096-brace-expansion-ii.py
+++ 1096-brace-expansion-ii/1096-brace-expansion-ii.py
@@ -1,33 +1,15 @@
 class Solution:
-  def braceExpansionII(self, expression: str) -> list[str]:
-    def merge(groups: list[list[str]], group: list[str]) -> None:
-      if not groups[-1]:
-        groups[-1] = group
-        return
+    def braceExpansionII(self, expression: str) -> List[str]:
+        def dfs(exp):
+            j = exp.find('}')
+            if j == -1:
+                s.add(exp)
+                return
+            i = exp.rfind('{', 0, j)
+            a, c = exp[:i], exp[j + 1 :]
+            for b in exp[i + 1 : j].split(','):
+                dfs(a + b + c)
 
-      groups[-1] = [word1 + word2 for word1 in groups[-1]
-                    for word2 in group]
-
-    def dfs(s: int, e: int) -> list[str]:
-      groups = [[]]
-      layer = 0
-
-      for i in range(s, e + 1):
-        c = expression[i]
-        if c == '{':
-          layer += 1
-          if layer == 1:
-            left = i + 1
-        elif c == '}':
-          layer -= 1
-          if layer == 0:
-            group = dfs(left, i - 1)
-            merge(groups, group)
-        elif c == ',' and layer == 0:
-          groups.append([])
-        elif layer == 0:
-          merge(groups, [c])
-
-      return sorted(list({word for group in groups for word in group}))
-
-    return dfs(0, len(expression) - 1)
+        s = set()
+        dfs(expression)
+        return sorted(s)
